096f2e21f504341264ed6d549c05b7fc9e52d5de
Nathan <nsafran1217@gmail.com>
2024-01-06 15:56:58 -0500

    implement fancy transition for display

--- lib/INS1Matrix/src/INS1Matrix.cpp
+++ lib/INS1Matrix/src/INS1Matrix.cpp
@@ -21,8 +21,13 @@ INS1Matrix::INS1Matrix(uint8_t dataPin, uint8_t clockPin, uint8_t latchPin, uint
     _displays = displays;
     _highValue = inverted ? LOW : HIGH; // if signals are inverted, swap the high and low levels we are going to use.
     _lowValue = inverted ? HIGH : LOW;
+    _lastDataSentToDisplay = new uint32_t[2 * displays];
 }
-
+INS1Matrix::~INS1Matrix()
+{
+    // Release dynamically allocated memory in the destructor
+    delete[] _lastDataSentToDisplay;
+}
 void INS1Matrix::writeStaticImgToDisplay(uint32_t imgData[]) // write 2 uint32_t to the display * the num of displays. imgData should be an array of uint32_t 2*displays big
 {
     uint8_t bitValue;
@@ -40,9 +45,9 @@ void INS1Matrix::writeStaticImgToDisplay(uint32_t imgData[]) // write 2 uint32_t
     digitalWrite(_latchPin, _highValue);
     delayMicroseconds(7);
     digitalWrite(_latchPin, _lowValue);
-    
+    memcpy(_lastDataSentToDisplay, imgData, sizeof(uint32_t) * _displays * 2); // for fancy transition
 }
-void INS1Matrix::setAnimationToDisplay(const uint32_t* animationImgData) // write 2 uint32_t to display * the num of displays, how many frames are in the animation data, the delay between frames in ms. imgData should be a const array of frames (array of uint32_t 2*displays big)
+void INS1Matrix::setAnimationToDisplay(const uint32_t *animationImgData) // write 2 uint32_t to display * the num of displays, how many frames are in the animation data, the delay between frames in ms. imgData should be a const array of frames (array of uint32_t 2*displays big)
 {
     _animationImgData = animationImgData;
 
@@ -50,21 +55,75 @@ void INS1Matrix::setAnimationToDisplay(const uint32_t* animationImgData) // writ
     _frames = (animationImgData[0] >> 16) & 0xFFFF;
     _timeOfLastFrame = millis();
     _lastFrame = 0;
-
 }
 void INS1Matrix::animateDisplay()
 {
-    
     uint32_t currentMillis = millis();
     if ((currentMillis - _timeOfLastFrame) >= _frameDelay)
     {
-        writeStaticImgToDisplay(const_cast<uint32_t *>(_animationImgData + 1 + (_lastFrame * _displays*2)));
-		_lastFrame++;
-		if (_lastFrame >= _frames)
-		{
-			_lastFrame = 0;
-		}
+        writeStaticImgToDisplay(const_cast<uint32_t *>(_animationImgData + 1 + (_lastFrame * _displays * 2)));
+        _lastFrame++;
+        if (_lastFrame >= _frames)
+        {
+            _lastFrame = 0;
+        }
 
         _timeOfLastFrame = currentMillis;
+    }
+}
+void INS1Matrix::fancyTransitionFrame(const uint32_t *FirstFrameImgData, int TransitionMode, int delayms) // transition between the old frame and a new frame
+{
+    if (TransitionMode == VERTICAL_TRANSITION)
+    {
+        const uint32_t patternMask[6][4] = {
+            {0xffffffff, 0x0fffffff, 0xffffffff, 0x0fffffff},
+            {0xfffff000, 0x0fffffff, 0xfffff000, 0x0fffffff},
+            {0xff000000, 0x0fffffff, 0xff000000, 0x0fffffff},
+            {0x00000000, 0x0ffffff0, 0x00000000, 0x0ffffff0},
+            {0x00000000, 0x0fff0000, 0x00000000, 0x0fff0000},
+            {0x00000000, 0x00000000, 0x00000000, 0x00000000}};
+
+        for (int bitMaskPos = 0; bitMaskPos < 6; bitMaskPos++)
+        {
+            uint8_t bitValue;
+            for (int i = 0; i < _displays * 2; i++)
+            {
+                uint32_t ImgData = _lastDataSentToDisplay[i] & patternMask[bitMaskPos][i];
+                for (int j = 0; j < 32; j++)
+                {
+                    bitValue = _inverted ? !(ImgData & (1ul << j)) : !!(ImgData & (1ul << j));
+                    digitalWrite(_dataPin, bitValue);
+                    digitalWrite(_clockPin, _highValue);
+                    delayMicroseconds(7);
+                    digitalWrite(_clockPin, _lowValue);
+                }
+            }
+            digitalWrite(_latchPin, _highValue);
+            delayMicroseconds(7);
+            digitalWrite(_latchPin, _lowValue);
+            delay(delayms);
+        }
+
+        for (int bitMaskPos = 5; bitMaskPos >= 0; bitMaskPos--)
+        {
+            uint8_t bitValue;
+            for (int i = 0; i < _displays * 2; i++) // displays*2 becuase each display needs two uint32_t's
+            {
+
+                uint32_t ImgData = FirstFrameImgData[i] & patternMask[bitMaskPos][i];
+                for (int j = 0; j < 32; j++)
+                {
+                    bitValue = _inverted ? !(ImgData & (1ul << j)) : !!(ImgData & (1ul << j));
+                    digitalWrite(_dataPin, bitValue);
+                    digitalWrite(_clockPin, _highValue);
+                    delayMicroseconds(7);
+                    digitalWrite(_clockPin, _lowValue);
+                }
+            }
+            digitalWrite(_latchPin, _highValue);
+            delayMicroseconds(7);
+            digitalWrite(_latchPin, _lowValue);
+            delay(delayms);
+        }
     }
 }
--- lib/INS1Matrix/src/INS1Matrix.h
+++ lib/INS1Matrix/src/INS1Matrix.h
@@ -6,14 +6,18 @@ Nathan Safran - 9/10/2023
 #ifndef INS1Matrix_h
 #define INS1Matrix_h
 #include "Arduino.h"
+#define VERTICAL_TRANSITION  0
+#define ROTATE_TRANSITION 1
 
 class INS1Matrix
 {
   public:
     INS1Matrix(uint8_t dataPin, uint8_t clockPin, uint8_t latchPin, uint8_t blankPin, boolean inverted, uint8_t displays);
+    ~INS1Matrix();
     void writeStaticImgToDisplay(uint32_t imgData[]); // write 2 uint32_t to the display * the num of displays. imgData should be an array of uint32_t 2*displays big
     void setAnimationToDisplay(const uint32_t* animationImgData); // write 2 uint32_t to display * the num of displays, how many frames are in the animation data, the delay between frames in ms. imgData should be a const array of frames (array of uint32_t 2*displays big)
     void animateDisplay();
+    void fancyTransitionFrame(const uint32_t* FirstFrameImgDat, int TransitionMode, int delayms);
 
     
 
@@ -31,7 +35,7 @@ class INS1Matrix
     uint16_t _frameDelay;
     uint32_t _timeOfLastFrame;  //time since last frame was displayed
     uint16_t _lastFrame; //last frame sent to display
-    uint32_t _lastDataSentToDisplay[4] = {0}; //this is specific to my implementation of the displays, since my display needs 4 uint32_t send to build the image
+    uint32_t *_lastDataSentToDisplay; 
 
 };
 
